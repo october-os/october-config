72c3fa1675da8a56d00ed27ce1cbd1de5dce613a
ariannelafraise <76917463+ariannelafraise@users.noreply.github.com>
2026-02-16 18:46:40 -0500

    changed some paths

--- scripts/update-wall.sh
+++ scripts/update-wall.sh
@@ -2,12 +2,12 @@
 
 configDir=$HOME/.config
 
-randomWallpaper=$(ls "$configDir/wallpapers" | shuf -n 1)
+randomWallpaper=$(ls "$configDir/october-config/wallpapers" | shuf -n 1)
 
 mkdir -p /tmp/october-config
 touch "/tmp/october-config/lastwallpaper"
 
-if [ $(ls -1q ~/.config/wallpapers | wc -l) != "1" ]; then
+if [ $(ls -1q ~/.config/october-config/wallpapers | wc -l) != "1" ]; then
     while [ "$randomWallpaper" = $(cat /tmp/october-config/lastwallpaper) ]
     do
         randomWallpaper=$(ls "$configDir/wallpapers" | shuf -n 1)
@@ -16,7 +16,7 @@ fi
 
 echo -n "$randomWallpaper" > /tmp/october-config/lastwallpaper
 
-swww img --transition-type wave --transition-fps 120 --transition-duration 1.5 $configDir/wallpapers/$randomWallpaper
+swww img --transition-type wave --transition-fps 120 --transition-duration 1.5 $configDir/october-config/wallpapers/$randomWallpaper
 
 wal -n -q -i "$configDir/wallpapers/$randomWallpaper"
 cp $HOME/.cache/wal/quickshell-template.qml $configDir/quickshell/theme/Theme.qml
