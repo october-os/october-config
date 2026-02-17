14d019117d0f41dfec1a09bfcca4125e817010e1
Arianne <76917463+ariannelafraise@users.noreply.github.com>
2026-02-16 20:26:04 -0500

    9 do hyprlock configuration (#11)
    * Notif draft
    
    * Added timer to show notif for a limited time
    
    * Final draft
    
    * Animations on notifications
    
    * Fixed notification closing animation bug
    
    * Fixed animations and replaced it with an anim on opacity
    
    * Fixed warnings
    
    * renamed quickshell pywal template, added hyprland pywal template, added hyprlock config, fixed wallpaper script bug
    
    * changed some paths
    
    * removed useless file
    
    ---------
    
    Co-authored-by: efournierrobert <elliott1447@gmail.com>

--- scripts/update-wall.sh
+++ scripts/update-wall.sh
@@ -2,20 +2,23 @@
 
 configDir=$HOME/.config
 
-randomWallpaper=$(ls "$configDir/wallpapers" | shuf -n 1)
+randomWallpaper=$(ls "$configDir/october-config/wallpapers" | shuf -n 1)
 
 mkdir -p /tmp/october-config
 touch "/tmp/october-config/lastwallpaper"
 
-while [ "$randomWallpaper" = $(cat /tmp/october-config/lastwallpaper) ]
-do
-    randomWallpaper=$(ls "$configDir/wallpapers" | shuf -n 1)
-done
+if [ $(ls -1q ~/.config/october-config/wallpapers | wc -l) != "1" ]; then
+    while [ "$randomWallpaper" = $(cat /tmp/october-config/lastwallpaper) ]
+    do
+        randomWallpaper=$(ls "$configDir/wallpapers" | shuf -n 1)
+    done
+fi
 
 echo -n "$randomWallpaper" > /tmp/october-config/lastwallpaper
 
-swww img --transition-type wave --transition-fps 120 --transition-duration 1.5 $configDir/wallpapers/$randomWallpaper
+swww img --transition-type wave --transition-fps 120 --transition-duration 1.5 $configDir/october-config/wallpapers/$randomWallpaper
 
 wal -n -q -i "$configDir/wallpapers/$randomWallpaper"
-cp $HOME/.cache/wal/wallpaper-template.qml $configDir/quickshell/theme/Theme.qml
+cp $HOME/.cache/wal/quickshell-template.qml $configDir/quickshell/theme/Theme.qml
+cp $HOME/.cache/wal/hyprland-template.conf $configDir/hypr/colors.conf
 cp $HOME/.cache/wal/colors-kitty.conf ~/.config/kitty
