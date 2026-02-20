2aede9d16983ba83c7b611f5c57aaa2dd190aa6f
efournierrobert <elliott1447@gmail.com>
2026-02-20 04:24:55 -0500

    Fixed the hyprland program by adding it to the setup script and added a notice in the top level conf file

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Pull wallpaper
 defaultWallpaperLink="https://w.wallhaven.cc/full/m3/wallhaven-m3y1j9.png"
 
 curl $defaultWallpaperLink -o wallpapers/wallpaper.png
+
+# Setup Hyprland user config
+mkdir hypr/user
+touch hypr/user/hyprland.conf
