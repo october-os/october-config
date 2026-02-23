3e4c95609b6f74ec118eb527146f31c9b155f31c
efournierrobert <elliott1447@gmail.com>
2026-02-23 11:41:05 -0500

    Added pfp to script and modified the binding of hyprlock

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # Pull wallpaper
-defaultWallpaperLink="https://w.wallhaven.cc/full/m3/wallhaven-m3y1j9.png"
-
+defaultWallpaperLink="https://raw.githubusercontent.com/october-os/october-assets/refs/heads/main/october-wallpapers/logo-text-bottom-small.png"
 curl $defaultWallpaperLink -o wallpapers/wallpaper.png
+
+# Pull default profile picture
+defaultPfp="https://raw.githubusercontent.com/october-os/october-assets/refs/heads/main/october-logo-background.png"
+curl $defaultPfp -o profile_picture.jpg
 
 # Setup Hyprland user config
 mkdir hypr/user
